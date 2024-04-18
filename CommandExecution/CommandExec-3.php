08e9f46f1224d13e6fcfb98b2797dd16155aa07c
ac-sanketbanait <162424014+ac-sanketbanait@users.noreply.github.com>
2024-04-18 11:11:43 +0530

    Update CommandExec-3.php

--- CommandExecution/CommandExec-3.php
+++ CommandExecution/CommandExec-3.php
@@ -17,31 +17,7 @@
     </form>
   </div>
   <div style="background-color:#ecf2d0;padding:20px;border-radius:0px 0px 20px 20px" align="center">
-    <?php
-    if(isset($_GET["typeBox"])){
-      $target =$_GET["typeBox"];
-      $substitutions = array(
-        '&&'=>'',
-        '& ' => '',
-        '&& ' => '',
-        ';'  => '',
-        '|' => '',
-        '-'  => '',
-        '$'  => '',
-        '('  => '',
-        ')'  => '',
-        '`'  => '',
-        '||' => '',
-        '/' => '',
-        '\\' => '',
-      );
-      $target = str_replace(array_keys($substitutions),$substitutions,$target);
-      echo shell_exec($target);
-      if($_GET["typeBox"] == "flag")
-        echo "You did again! Impressive.";
-    }
-
-    ?>
+  
   </div>
   </body>
 </html>
